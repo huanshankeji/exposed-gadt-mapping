f0929bb03d1e4a111b88456d8138f468b98919ca
Yongshun Ye <ShreckYe@gmail.com>
2025-09-19 16:23:34 +0800

    Bump `kotlin-common` to the latest snapshot too

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -3,7 +3,7 @@ import com.huanshankeji.CommonVersions
 
 val projectVersion = "0.3.1-SNAPSHOT"
 
-// don't use a snapshot version in a main branch
+// TODO don't use a snapshot version in a main branch
 // TODO remove Exposed's explicit version when migration to Exposed 1.0.0 is complete
-val commonVersions = CommonVersions(kotlinCommon = "0.6.1", exposed = "0.61.0")
+val commonVersions = CommonVersions(kotlinCommon = "0.7.0-SNAPSHOT", exposed = "0.61.0")
 val commonDependencies = CommonDependencies(commonVersions)
